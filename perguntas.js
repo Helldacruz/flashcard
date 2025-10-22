441b6f6dad7760ec2d12c89aa55ad3b2af03e736
Helldacruz <deivid.cruz.campos@escola.pr.gov.br>
2025-10-22 01:20:33 +0000

    002

--- perguntas.js
+++ perguntas.js
@@ -11,13 +11,55 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Matematica',
+    'Quais são as 5 matemáticas?',
+    'Suas principais áreas de estudo são a aritmética, álgebra, geometria e estatística.'
 )
 
 criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Química',
+    'Qual a carga elétrica de cada partícula?',
+    'Próton: positiva; Elétron: negativa; Nêutron: neutra. '
+)
+
+criaCartao(
+    'Ed.Financeira',
+    'Quais são as 5 palavras importantes para educação financeira?',
+    'Orçamento,Planejamento,Economia,Investimento e Dívida'
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'Quais são as 4 fases da língua portuguesa?',
+    'Português Arcaico, Português Moderno, Português Clássico e Português Contemporâneo'
+)
+
+criaCartao(
+    'Resolução de Problemas',
+    'Quais são as 4 etapas de resolução de problemas?',
+    'Polya (1978) dividia o processo de resolução de um problema em quatro etapas: compreensão do problema; construção de uma estratégia de resolução; execução da estratégia e revisão da solução.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Sociologia',
+    'Quais são os 5 pilares da sociologia?',
+    'Os cinco elementos centrais da sociologia podem ser definidos de diferentes formas, mas frequentemente incluem estrutura social, socialização, cultura, instituições sociais e mudança social. '
+)
+
+criaCartao(
+    '',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
